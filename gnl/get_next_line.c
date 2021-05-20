4c9cda7567136bde45b738d3648a9628ff7b2110
Telly Phlogiston <tphlogis@un-i4.kzn.21-school.ru>
2021-05-20 11:38:43 +0300

    finished 'netwhat' and 'get_next_line'

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -1,172 +1,58 @@
 #include "get_next_line.h"
-#include <stdio.h>
 
-char* find_str_start_end(char* str, char direction)
+int	gnl(char **line, char buf[], int i, char *nl)
 {
-	if (!(*str))
-		return (str);
-	while (*str)
-		str += direction;
-	return (str);
-}
-
-char *shift_buf_to_nl(char* buf)
-{
-	char* nl_chr_pos;
-	nl_chr_pos = ft_memchr(buf, '\n', BUFFER_SIZE);
-	if (nl_chr_pos != NULL)
+	nl = ft_memchr(buf, '\n', BUFFER_SIZE);
+	if (i == 0)
+		free(*line);
+	if (nl == NULL)
 	{
-		ft_memmove(buf, nl_chr_pos + 1, buf + BUFFER_SIZE - nl_chr_pos - 1);
-		ft_memset(buf + (buf + BUFFER_SIZE - nl_chr_pos - 1), 0,
-			BUFFER_SIZE - ((buf + BUFFER_SIZE - nl_chr_pos - 1)));
-	}
-	return (buf);
-}
-
-int get_next_line(int fd, char **line)
-{
-	int read_ret;
-	int res;
-	static char buf[BUFFER_SIZE + 1];
-	char* nl_chr_pos;
-	int i;
-
-	if (buf[0] != 0)
-	{
-		read_ret = 1;
-		i = 0;
-		res = -1;
-		while (read_ret != 0)
-		{
-			if (ft_memchr(buf, '\n', BUFFER_SIZE) == NULL)
-			{
-				res = 0;
-				if (i == 0)
-				{
-					free(*line);
-					*line = ft_strjoin(NULL, buf);
-				}
-				else
-					*line = ft_strjoin(*line, buf);
-				ft_memset(buf, 0, BUFFER_SIZE);
-				read_ret = read(fd, buf, BUFFER_SIZE);
-				i++;
-			}
-			else
-			{
-				res = 1;
-				nl_chr_pos = ft_memchr(buf, '\n', BUFFER_SIZE);
-				nl_chr_pos[0] = 0;
-				if (i == 0)
-				{
-					free(*line);
-					*line = ft_strjoin(NULL, buf);
-				}
-				else
-					*line = ft_strjoin(*line, buf);
-				nl_chr_pos[0] = '\n';
-				shift_buf_to_nl(buf);
-				if (buf + BUFFER_SIZE != find_str_start_end(buf, 1))
-					read_ret = read(fd, find_str_start_end(buf, 1),
-						buf + BUFFER_SIZE - find_str_start_end(buf, 1));
-				break;
-			}
-		}
-		if (read_ret == 0 && res == 0 && i == 0)
-		{
-				*line = malloc(1);
-				**line = '\0';
-		}
+		if (i == 0)
+			*line = ft_strjoin(NULL, buf);
+		else
+			*line = ft_strjoin(*line, buf);
+		ft_memset(buf, 0, BUFFER_SIZE);
+		return (0);
 	}
 	else
 	{
-		read_ret = read(fd, buf, BUFFER_SIZE);
-		if (read_ret == -1 || line == NULL)
-			return (-1);
-		buf[read_ret] = 0;
-		*line = NULL;
-		return (get_next_line(fd, line));
+		nl[0] = 0;
+		if (i == 0)
+			*line = ft_strjoin(NULL, buf);
+		else
+			*line = ft_strjoin(*line, buf);
+		ft_memmove(buf, nl + 1, buf + BUFFER_SIZE - nl - 1);
+		ft_memset(buf + (buf + BUFFER_SIZE - nl - 1), 0,
+			BUFFER_SIZE - ((buf + BUFFER_SIZE - nl - 1)));
+		return (1);
 	}
-	if (read_ret == 0 && buf[0] != 0)
-		res = 1;
-	if (read_ret > 1)
-		res = 1;
-	return (res);
 }
 
-#include <fcntl.h>
-int main()
+int	get_next_line(int fd, char **line)
 {
-//	char* line;
-//	int fd;
-//	int gnl_ret = 0;
-//
-//	fd = open("/Users/tphlogis/Documents/My_projects/gnl/42TESTERS-GNL-master/files/alphabet", O_RDWR);
-//	while ((gnl_ret = get_next_line(fd, &line)) == 1)
-//	{
-//		printf("%s %d\n", line, gnl_ret);
-//	}
-//	printf("%s %d\n", line, gnl_ret);
-//	close(fd);
-	int             fd;
-	int             i;
-	int             j;
-	char    		*line = 0;
-	char			*lineadress[66];
+	int			rr;
+	static char	buf[BUFFER_SIZE + 1];
+	int			gnl_ret;
+	int			i;
 
-	j = 1;
-	printf("\n==========================================\n");
-	printf("========== TEST 1 : The Alphabet =========\n");
-	printf("==========================================\n\n");
-
-	if (!(fd = open("/Users/tphlogis/Documents/My_projects/gnl/42TESTERS-GNL-master/files/alphabet", O_RDONLY)))
+	if (line == NULL || read(fd, buf, 0) < 0 || BUFFER_SIZE < 0)
+		return (-1);
+	rr = 1;
+	if (buf[0] == 0)
+		rr = read(fd, buf, BUFFER_SIZE);
+	*line = NULL;
+	if (rr == 0)
 	{
-		printf("\nError in open\n");
+		*line = ft_memset(malloc(1), 0, 1);
 		return (0);
 	}
-	while ((i = get_next_line(fd, &line)) > 0)
+	i = 0;
+	gnl_ret = 0;
+	while (rr != 0 && gnl_ret != 1)
 	{
-		printf("|%s\n", line);
-		lineadress[j - 1] = line;
-		j++;
+		gnl_ret = gnl(line, buf, i++, ft_memchr(buf, '\n', BUFFER_SIZE));
+		if (BUFFER_SIZE != ft_strlen(buf))
+			rr = read(fd, buf + ft_strlen(buf), BUFFER_SIZE - ft_strlen(buf));
 	}
-	printf("|%s\n", line);
-	free(line);
-	close(fd);
-	if (i == -1)
-		printf ("\nError in Fonction - Returned -1\n");
-	else if (j == 66)
-		printf("\nRight number of lines\n");
-	else if (j != 66)
-		printf("\nNot Good - Wrong Number Of Lines\n");
-//	while (--j > 0)
-//		free(lineadress[j - 1]);
-	j = 1;
-
-	printf("\n==========================================\n");
-	printf("========= TEST 2 : Empty Lines ===========\n");
-	printf("==========================================\n\n");
-
-	if (!(fd = open("files/empty_lines", O_RDONLY)))
-	{
-		printf("\nError in open\n");
-		return (0);
-	}
-	while ((i = get_next_line(fd, &line)) > 0)
-	{
-		printf("|%s\n", line);
-		free(line);
-		j++;
-	}
-	printf("|%s\n", line);
-	free(line);
-	close(fd);
-
-	if (i == -1)
-		printf ("\nError in Fonction - Returned -1\n");
-	else if (j == 9)
-		printf("\nRight number of lines\n");
-	else if (j != 9)
-		printf("\nNot Good - Wrong Number Of Lines\n");
-	j = 1;
+	return (rr || gnl_ret);
 }
--- gnl/get_next_line.h
+++ gnl/get_next_line.h
@@ -2,7 +2,7 @@
 # define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 42
+#  define BUFFER_SIZE 100
 # endif
 
 # ifdef BUFFER_SIZE
@@ -15,7 +15,6 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-
 int		get_next_line(int fd, char **line);
 char	*ft_strjoin(char const *s1, char const *s2);
 void	*ft_memchr(const void *s, int c, size_t n);
@@ -24,23 +23,3 @@ void	*ft_memmove(void *dst, const void *src, size_t len);
 size_t	ft_strlen(const char *s);
 
 #endif
-
-//		nl_chr_pos = NULL;
-//		while (read_ret != 0 && nl_chr_pos == NULL)
-//		{
-//			nl_chr_pos = ft_memchr(buf, '\n', BUFFER_SIZE);
-//			if (nl_chr_pos != NULL)
-//			{
-//				nl_chr_pos[0] = 0;
-//				*line = ft_strjoin(NULL, buf);
-//				nl_chr_pos[0] = '\n';
-//				shift_buf_to_nl(buf);
-//				break;
-//			}
-//			else
-//			{
-//				*line = ft_strjoin(*line, buf);
-//			}
-//			read_ret = read(fd, buf, BUFFER_SIZE);
-//			ft_memset(buf + read_ret, 0, BUFFER_SIZE - read_ret);
-//		}
