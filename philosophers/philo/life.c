ac8effe3a231476ffd0c81ab822815f82c3d6ba3
Telly Phlogiston <tphlogis@ev-f5.kzn.21-school.ru>
2021-09-12 18:20:15 +0300

    philosophers: almost finished bonus part

--- philosophers/philo/life.c
+++ philosophers/philo/life.c
@@ -13,7 +13,8 @@ int	check_state(t_philo *phil)
 		phil->state = DEAD;
 		return (DEAD);
 	}
-	if (phil->eat_counter == phil->input->times_eat)
+	if (phil->eat_counter == phil->input->times_eat
+		&& phil->input->philos_nbr != 1)
 	{
 		phil->state = ALL_EATEN;
 		return (ALL_EATEN);
@@ -23,28 +24,21 @@ int	check_state(t_philo *phil)
 
 void	eat(t_philo *phil)
 {
-	if (phil->philo_id % 2)
-		checkerr(phil, pthread_mutex_lock(phil->left_fork), LOCK_ERROR);
-	else
-		checkerr(phil, pthread_mutex_lock(phil->right_fork), LOCK_ERROR);
+	checkerr(phil, pthread_mutex_lock(phil->left_fork), LOCK_ERROR);
 	print_state(phil, "has taken fork", "\x1b[32m", LIVE);
-	if (phil->philo_id % 2)
+	if (phil->input->philos_nbr != 1)
+	{
 		checkerr(phil, pthread_mutex_lock(phil->right_fork), LOCK_ERROR);
-	else
-		checkerr(phil, pthread_mutex_lock(phil->left_fork), LOCK_ERROR);
-	print_state(phil, "has taken fork", "\x1b[32m", LIVE);
-	print_state(phil, "is eating", "\x1b[92m", LIVE);
-	phil->last_eat_time = get_time();
-	phil->eat_counter++;
-	go_sleep(phil->input->time_to_eat, phil);
-	if (phil->philo_id % 2)
-		checkerr(phil, pthread_mutex_unlock(phil->left_fork), UNLOCK_ERROR);
-	else
+		print_state(phil, "has taken fork", "\x1b[32m", LIVE);
+		print_state(phil, "is eating", "\x1b[92m", LIVE);
+		phil->last_eat_time = get_time();
+		phil->eat_counter++;
+		go_sleep(phil->input->time_to_eat, phil);
 		checkerr(phil, pthread_mutex_unlock(phil->right_fork), UNLOCK_ERROR);
-	if (phil->philo_id % 2)
-		checkerr(phil, pthread_mutex_unlock(phil->right_fork), UNLOCK_ERROR);
+	}
 	else
-		checkerr(phil, pthread_mutex_unlock(phil->left_fork), UNLOCK_ERROR);
+		go_sleep(phil->input->time_to_die, phil);
+	checkerr(phil, pthread_mutex_unlock(phil->left_fork), UNLOCK_ERROR);
 }
 
 void	*life(void *arg)
@@ -53,11 +47,13 @@ void	*life(void *arg)
 
 	phil = (t_philo *)arg;
 	pthread_detach(phil->thread_id);
+	if (phil->philo_id % 2)
+		usleep(100);
 	while (phil->state == LIVE)
 	{
 		eat(phil);
 		if (check_state(phil))
-			return (NULL);
+			break ;
 		print_state(phil, "is sleeping", "\x1b[94m", LIVE);
 		go_sleep(phil->input->time_to_sleep, phil);
 		if (phil->state != LIVE)
